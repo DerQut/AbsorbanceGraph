81d3d59463eeba9efff65a4879cf25cdb77b8929
Marcel Chołodecki <cholodecki@gmail.com>
2025-03-14 16:17:48 +0100

    Initial Commit

---
+++ AbsorbanceGraph/AbsorbanceGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsorbanceGraphApp.swift
+//  AbsorbanceGraph
+//
+//  Created by Marcel Chołodecki on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AbsorbanceGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
